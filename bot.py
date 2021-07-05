62cf5776f729fe641a81852e84b9c6af92a2e580
FlappyWisteria <bepypaverniy@gmail.com>
2021-07-05 22:48:44 +0900

    sing

--- bot.py
+++ bot.py
@@ -50,10 +50,92 @@ async def baki(ctx):
 async def happy(ctx):
     await ctx.send(str("誕生日は終わったよ !bakiで烈海王がしゃべるよ"))
 
+youtube_dl.utils.bug_reports_message = lambda: ''
+
+ytdl_format_options = {
+    'format': 'bestaudio/best',
+    'outtmpl': '%(extractor)s-%(id)s-%(title)s.%(ext)s',
+    'restrictfilenames': True,
+    'noplaylist': True,
+    'nocheckcertificate': True,
+    'ignoreerrors': False,
+    'logtostderr': False,
+    'quiet': True,
+    'no_warnings': True,
+    'default_search': 'auto',
+    'source_address': '0.0.0.0' # bind to ipv4 since ipv6 addresses cause issues sometimes
+}
+
+ffmpeg_options = {
+    'options': '-vn'
+}
+
+ytdl = youtube_dl.YoutubeDL(ytdl_format_options)
+
+
+class YTDLSource(discord.PCMVolumeTransformer):
+    def __init__(self, source, *, data, volume=0.5):
+        super().__init__(source, volume)
+
+        self.data = data
+
+        self.title = data.get('title')
+        self.url = data.get('url')
+
+    @classmethod
+    async def from_url(cls, url, *, loop=None, stream=False):
+        loop = loop or asyncio.get_event_loop()
+        data = await loop.run_in_executor(None, lambda: ytdl.extract_info(url, download=not stream))
+
+        if 'entries' in data:
+            # take first item from a playlist
+            data = data['entries'][0]
+
+        filename = data['url'] if stream else ytdl.prepare_filename(data)
+        return cls(discord.FFmpegPCMAudio(filename, **ffmpeg_options), data=data)
+
+
+class Music(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+    
+    @commands.command()
+    async def play(self, ctx, *, url):
+        """Plays from a url (almost anything youtube_dl supports)"""
+
+        async with ctx.typing():
+            player = await YTDLSource.from_url(url, loop=self.bot.loop)
+            ctx.voice_client.play(player, after=lambda e: print('Player error: %s' % e) if e else None)
+
+        await ctx.send('再生中: {}'.format(player.title))
+
+    @commands.command(aliases=["bye","disconnect"])
+    async def stop(self, ctx):
+        channel = ctx.author.voice.channel
+        if channel is None:
+            return await ctx.send(VCに接続していません)
+
+        await ctx.voice_client.disconnect()
+        await ctx.send("VCから離脱しました。")
+
+    @play.before_invoke
+
+    async def ensure_voice(self, ctx):
+        if ctx.voice_client is None:
+            if ctx.author.voice:
+                await ctx.author.voice.channel.connect()
+            else:
+                await ctx.send("ボイスチャンネルに接続してないよ")
+                raise commands.CommandError("ボイスチャンネルに接続してないよ")
+        elif ctx.voice_client.is_playing():
+            ctx.voice_client.stop()
+
 @bot.event
 async def on_ready():
     print('Logged in as {0} ({0.id})'.format(bot.user))
     print('------')
 
+
+bot.add_cog(Music(bot))
 
 bot.run(TOKEN)
